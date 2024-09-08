f9e02e39fb0610dceec83519aab7f9b223d0b62c
Muhammad Ahmad Sharafat <112656643+AhmadSharafat@users.noreply.github.com>
2024-09-08 19:21:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#datepicker").datepicker({
+    format: "mm/dd/yyyy",
+    autoclose: true,
+    orientation: "bottom", // Ensures the calendar appears below
+  });
+});
